95b01d5056820046bf88b044bb5661946e487bcf
mathusan paramanithy <mathuyt141@gmail.com>
2025-10-25 15:32:27 +0100

    feat(favouriteStop.js): Added ability to view favourited bus stops

--- src/commands/bus/favStop.js
+++ src/commands/bus/favStop.js
@@ -17,7 +17,7 @@ module.exports = {
         .setDescription('Shows the latency for the bot!'),
 
     error: false,
-    execute: ({ interaction, client }) => {
+    execute: async ({ interaction, client }) => {
 
         const pool = mysql.createPool({
             host: process.env.dbHost,
@@ -36,20 +36,59 @@ module.exports = {
         const userId = interaction.user.id;
 
         function fetchFav(userId) {
-            pool.query(`SELECT * FROM userInfo WHERE uid = ${userId}`, function (err, results) {
-                if (err) {
-                    console.error(err);
-                }
-
-                return results
-
+            return new Promise((resolve, reject) => {
+                pool.query(`SELECT stopId FROM userFavourites WHERE uid = ?`, [userId], (err, results) => {
+                    if (err) {
+                        console.error(err);
+                        reject(err);
+                    } else {
+                        resolve(results);
+                    }
+                });
             });
         }
 
-        const results = fetchFav(userId);
+
+        const results = await fetchFav(userId);
         if (!results) {
             return interaction.followUp('It seems you do not have a favourite bus stop.')
         }
+
+        const stopId = results[0].stopId
+
+        const arrivalsRes = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_key=${process.env.tflapi}`);
+        const arrivals = await arrivalsRes.json();
+
+        if (!arrivals.length) {
+            return interaction.editReply({
+                content: '🚫 No upcoming buses at this stop.',
+                components: []
+            });
+        }
+        // Fetching requested bus stop details.
+        const sortedArrivals = arrivals
+            .sort((a, b) => a.timeToStation - b.timeToStation)
+            .slice(0, 5)
+            .map(bus => `• **${bus.lineName}** to *${bus.destinationName}* — arriving in **${Math.round(bus.timeToStation / 60)} min**`);
+
+        const stopLetter = arrivals?.stopLetter || arrivals?.indicator?.replace(/^Stop\s+/i, "") || null;
+        const stopName = arrivals?.commonName || arrivals[0].stationName;
+
+
+        // Creating the embed
+        const embed = new EmbedBuilder()
+            .setTitle(stopLetter
+                ? `🚌 Next buses at ${stopName} (${stopLetter})`
+                : `🚌 Next buses at ${stopName}`)
+            .setDescription(sortedArrivals.join('\n'))
+            .setColor(0x4caf50)
+            .setFooter({ text: 'Data provided by TFL and OS Maps.' })
+            .setTimestamp();
+
+        await interaction.editReply({
+            embeds: [embed]
+        });
+
 
 
 
